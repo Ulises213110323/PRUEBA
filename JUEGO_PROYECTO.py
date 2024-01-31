1c9fe84ba82040d5a36029b08704eb24ed449553
Sheila Olguin Trejo <hyakuyamisaki4@gmail.com>
2024-01-30 22:49:45 -0600

    Se agrega la primera modificacion )Pedir nombre a usuario

--- JUEGO_PROYECTO.py
+++ JUEGO_PROYECTO.py
@@ -11,14 +11,21 @@ class JuegoAdivinaNumero:
             return "Demasiado bajo. Intenta de nuevo."
         else:
             return "Demasiado alto. Intenta de nuevo."
+        
+    def obtener_nombre_usuario():
+        nombre = input ("Hola! Cual es tu nombre?")
+        return nombre
+    
+    nombre_usuario = obtener_nombre_usuario()
+    print(f"Hola {nombre_usuario}, vamos a jugar a adivinar el número XD")
 
-# Generar un número secreto aleatorio entre 1 y 100
+
 numero_secreto = random.randint(1, 100)
 
-# Crear una instancia del juego
+
 juego = JuegoAdivinaNumero(numero_secreto)
 
-# Jugar hasta adivinar el número
+
 while True:
     intento = int(input("Intenta adivinar el número: "))
     resultado = juego.adivinar(intento)
